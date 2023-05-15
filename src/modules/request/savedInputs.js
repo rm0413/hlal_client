dbb2448d1e7b7f34d0bf5bcf78b390bdf1ef6ee3
Sorisantos.r21 <reinamae.sorisantos@fujitsu.com>
2023-05-15 10:07:59 +0800

    [Saved Inputs] Added: #3105 [Template]

--- src/modules/request/savedInputs.js
+++ src/modules/request/savedInputs.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 export const useSavedInputsStore = defineStore({
-    id: 'editItemDetails',
+    id: 'savedInputs',
     state: () => ({
         savedInputs: [],
         savedInputsFields: [
