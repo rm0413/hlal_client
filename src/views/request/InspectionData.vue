2f513460f8d27f4329397ce0a64ab74061e68d92
Sorisantos.r21 <reinamae.sorisantos@fujitsu.com>
2023-05-15 15:35:18 +0800

    [Inspction Data] Added: 2992 [Template]

--- src/views/request/InspectionData.vue
+++ src/views/request/InspectionData.vue
@@ -7,24 +7,24 @@
           Inspection Data
         </label>
         <div class="flex gap-2">
-          <div class="flex flex-row">
-            <select class="text-center p-1 border-2 rounded w-[12rem]" />
-            <button class="h-full bg-[#A10E13] text-white py-1 px-3 rounded">
-              <font-awesome-icon icon="plus-square"></font-awesome-icon>
-              New Unit
-            </button>
+          <div class="flex">
+            <label class="flex flex-col items-center justify-center">Search by:  </label>
+            <div class="h-full bg-[#A10E13] text-white py-1 px-3 rounded-l-md ml-3">
+              Code
+            </div>
+            <div class="text-center p-1 border-2 rounded w-[8rem]">Part Number</div>
           </div>
           <div class="relative">
             <i class="h-full z-50 text-gray-400 top-[2px] py-1 px-3 rounded absolute"
               ><font-awesome-icon icon="magnifying-glass"></font-awesome-icon
             ></i>
-            <input class="text-center p-1 border-2 rounded" />
-            <button class="h-full bg-gray-400 text-white py-1 px-3 rounded">
+            <input class="text-center p-1 border-2 rounded-l-md" />
+            <button class="h-full bg-gray-400 text-white py-1 px-3 rounded-r-md">
               Search
             </button>
           </div>
           <div>
-            <select class="text-center p-1 border-2 rounded w-[12rem]" />
+            <select class="text-center p-1 border-2 rounded-md w-[12rem]" />
           </div>
         </div>
       </div>
