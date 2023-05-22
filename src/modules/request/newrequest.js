dab764cd6c2f2c07bba06d6d6d051c25d960b297
Sorisantos.r21 <reinamae.sorisantos@fujitsu.com>
2023-05-23 07:14:21 +0800

    [Request Form][New Request] Added: #2989 View Item Details Modal - Load All Agreement List Requests

--- src/modules/request/newrequest.js
+++ src/modules/request/newrequest.js
@@ -1,14 +1,17 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useNewRequestStore = defineStore({
     id: 'newRequest',
     state: () => ({
+        agreement_list: [],
         view_item_details_fields: [
-            {label: 'Selected', key: 'select'},
+            {label: '#', key: '#'},
+            {label: 'Selected', key: 'selected'},
             {label: 'Trial Number', key: 'trial_number'},
             {label: 'Request Date', key: 'request_date'},
-            {label: 'TRI No.', key: 'tri_no'},
-            {label: 'Supplier', key: 'supplier'},
+            {label: 'TRI No.', key: 'tri_number'},
+            {label: 'Supplier', key: 'supplier_name'},
             {label: 'Part Number', key: 'part_number'},
             {label: 'Revision', key: 'revision'},
             {label: 'Dimension', key: 'dimension'},
@@ -16,11 +19,27 @@ export const useNewRequestStore = defineStore({
         ]
     }),
     actions: {
+        setAgreementList(){
+            return new Promise((resolve, reject) => {
+                axios.get('agreement-list').then(response => {
+                    resolve(response.data.data)
+                    // console.log(response.data)
+                    this.agreement_list = response.data.data
+                }).catch(err => {
+                    reject(err)
+                })
+            })
+        }
 
     },
     getters: {
         getViewItemDetailsFields(){
             return this.view_item_details_fields
+        },
+        getAgreementList(){
+            // alert(1)
+            // console.log(this.agreement_list)
+            return this.agreement_list
         }
     }
 })
--- src/views/request/NewRequest.vue
+++ src/views/request/NewRequest.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="h-[89vh] w-full grid grid-cols-9 min-[100px]:overflow-y-scroll lg:overflow-y-hidden gap-2"
-  >
+  <div class="h-[89vh] w-full grid grid-cols-9 min-[100px]:overflow-y-scroll lg:overflow-y-hidden gap-2">
     <div class="lg:col-span-7 min-[100px]:col-span-9 h-full flex flex-col">
       <div class="flex justify-between">
         <label class="text-[24px] tracking-widest font-bold text-gray-600 font-mono">
@@ -10,15 +8,15 @@
         <div class="flex gap-2 mr-10">
           <div class="flex flex-row">
             <select class="text-center p-1 border-2 rounded w-[12rem] rounded-l-md" />
-            <button @click="openModal('new_unit')" class="h-full bg-[#A10E13] text-white py-1 px-3 rounded-r-md hover:bg-red-600">
+            <button @click="openModal('new_unit')"
+              class="h-full bg-[#A10E13] text-white py-1 px-3 rounded-r-md hover:bg-red-600">
               <font-awesome-icon icon="plus-square"></font-awesome-icon>
               New Unit
             </button>
           </div>
           <div class="relative">
-            <i class="h-full z-50 text-gray-400 top-[2px] py-1 px-3 rounded absolute"
-              ><font-awesome-icon icon="magnifying-glass"></font-awesome-icon
-            ></i>
+            <i class="h-full z-50 text-gray-400 top-[2px] py-1 px-3 rounded absolute"><font-awesome-icon
+                icon="magnifying-glass"></font-awesome-icon></i>
             <input class="text-center p-1 border-2 rounded-l-md" />
             <button @click="openModal('search')" class="h-full bg-gray-400 text-white py-1 px-3 rounded-r-md">
               Search
@@ -30,103 +28,61 @@
         <div class="col-span-1 p-10">
           <label class="flex flex-col gap-2">
             Trial No.
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Request Date
-            <input
-              type="date"
-              class="w-full text-center border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="date" class="w-full text-center border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Additional Request Qty Date
-            <input
-              type="date"
-              class="w-full text-center border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="date" class="w-full text-center border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             TRI No.
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             TRI Quantity
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Request Person
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Superior Approval
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
         </div>
         <div class="col-span-1 p-10">
           <label class="flex flex-col gap-2">
             Supplier
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Part Number
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Sub Part Number
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Revision
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Coordinates
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Dimension
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <label class="flex flex-col gap-2">
             Actual Value
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
         </div>
         <div class="col-span-1 p-10">
@@ -144,17 +100,11 @@
           </label>
           <label class="flex flex-col gap-2">
             Request Value
-            <textarea
-              style="resize: none"
-              class="w-full border-2 rounded p-1 hover:border-blue-300 h-[6.5rem]"
-            />
+            <textarea style="resize: none" class="w-full border-2 rounded p-1 hover:border-blue-300 h-[6.5rem]" />
           </label>
           <label class="flex flex-col gap-2">
             Request Quantity
-            <input
-              type="text"
-              class="w-full border-2 rounded p-1 hover:border-blue-300"
-            />
+            <input type="text" class="w-full border-2 rounded p-1 hover:border-blue-300" />
           </label>
           <div class="flex justify-center items-center mt-8 gap-2">
             <button type="submit" class="bg-[#A10E13] text-white p-1 w-[7rem] rounded">
@@ -168,38 +118,28 @@
       </form>
     </div>
     <div class="lg:col-span-2 min-[100px]:col-span-9 h-full flex flex-col mt-10 gap-2">
-      <button
-        @click="openModal('multiple_input')"
+      <button @click="openModal('multiple_input')"
         class="w-full p-3 flex justify-center items-center bg-[#A10E13] text-white rounded hover:bg-red-600"
-        data-open-modal
-      >
+        data-open-modal>
         Multiple Input
       </button>
-      <button
-        @click="openModal('view_items')"
-        class="w-full p-3 flex justify-center items-center bg-green-600 text-white rounded hover:bg-green-500"
-      >
+      <button @click="openModal('view_items')"
+        class="w-full p-3 flex justify-center items-center bg-green-600 text-white rounded hover:bg-green-500">
         View Item Details
       </Button>
     </div>
     <!--Multiple Input-->
-    <dialog
-      ref="multiple_input"
-      class="p-0 rounded transform duration-300 -translate-y-5"
-    >
+    <dialog ref="multiple_input" class="p-0 rounded transform duration-300 -translate-y-5">
       <div class="flex flex-col">
-        <div
-          class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]"
-        >
+        <div class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]">
           <span>Multiple Input</span>
-          <button
-            class="px-3 py-2 rounded-full hover:bg-red-600"
-            @click="closeModal('multiple_input')"
-          >
+          <button class="px-3 py-2 rounded-full hover:bg-red-600" @click="closeModal('multiple_input')">
             <font-awesome-icon icon="xmark"></font-awesome-icon>
           </button>
         </div>
-        <div class="flex p-5"><FileUploader></FileUploader></div>
+        <div class="flex p-5">
+          <FileUploader></FileUploader>
+        </div>
         <button class="p-3 bg-green-600 text-white hover:bg-green-500">
           Download Format
         </button>
@@ -207,49 +147,44 @@
       </div>
     </dialog>
     <!--View Items-->
-    <dialog
-      ref="view_items"
-      class="p-0 rounded transform duration-300 -translate-y-5"
-    >
+    <dialog ref="view_items" class="p-0 rounded transform duration-300 -translate-y-5">
       <div class="flex flex-col">
-        <div
-          class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]"
-        >
+        <div class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]">
           <span>View Item Details</span>
-          <button
-            class="px-3 py-2 rounded-full hover:bg-red-600"
-            @click="closeModal('view_items')"
-          >
+          <button class="px-3 py-2 rounded-full hover:bg-red-600" @click="closeModal('view_items')">
             <font-awesome-icon icon="xmark"></font-awesome-icon>
           </button>
         </div>
         <div class="flex p-5">
-          <CTable :fields="newRequestStore.getViewItemDetailsFields"></CTable>
+          <CTable :fields="newRequestStore.getViewItemDetailsFields" :items="newRequestStore.getAgreementList">
+            <template #cell(#)="data">
+              <div class="flex items-center justify-center">
+                {{ data.index + 1 }}
+              </div>
+            </template>
+            <template #cell(selected)="data">
+              <div class="flex items-center justify-center">
+                <input type="checkbox" name="chk" id="chk">
+              </div>
+            </template>
+          </CTable>
         </div>
         <button class="p-3 bg-[#A10E13] text-white hover:bg-red-600">Generate</button>
       </div>
     </dialog>
     <!-- New Unit -->
-    <dialog
-      ref="new_unit"
-      class="p-0 rounded transform duration-300 -translate-y-5"
-    >
+    <dialog ref="new_unit" class="p-0 rounded transform duration-300 -translate-y-5">
       <div class="flex flex-col">
-        <div
-          class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]"
-        >
+        <div class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]">
           <span>Create New Unit</span>
-          <button
-            class="px-3 py-2 rounded-full hover:bg-red-600"
-            @click="closeModal('new_unit')"
-          >
+          <button class="px-3 py-2 rounded-full hover:bg-red-600" @click="closeModal('new_unit')">
             <font-awesome-icon icon="xmark"></font-awesome-icon>
           </button>
         </div>
         <div class="flex p-5">
           <label class="flex flex-col">
             <span>Name of Unit</span>
-            <input class="w-[20rem] h-10 border-2 p-2 hover:border-red-600 rounded"/>
+            <input class="w-[20rem] h-10 border-2 p-2 hover:border-red-600 rounded" />
           </label>
         </div>
         <button class="p-3 bg-[#A10E13] text-white hover:bg-red-600">Save</button>
@@ -257,24 +192,17 @@
       </div>
     </dialog>
     <!--Search-->
-    <dialog
-      ref="search"
-      class="p-0 rounded transform duration-300 -translate-y-5"
-    >
+    <dialog ref="search" class="p-0 rounded transform duration-300 -translate-y-5">
       <div class="flex flex-col">
-        <div
-          class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]"
-        >
+        <div class="flex justify-between items-center h-[5vh] px-3 text-white bg-[#A10E13]">
           <span>Search</span>
-          <button
-            class="px-3 py-2 rounded-full hover:bg-red-600"
-            @click="closeModal('search')"
-          >
+          <button class="px-3 py-2 rounded-full hover:bg-red-600" @click="closeModal('search')">
             <font-awesome-icon icon="xmark"></font-awesome-icon>
           </button>
         </div>
-        <div class="flex p-5 h-[30vh] overflow-y-scroll">
-          <CTable :fields="newRequestStore.getViewItemDetailsFields"></CTable>
+        <div class="flex p-5 overflow-y-scroll">
+          <c-table :fields="newRequestStore.view_item_details_fields">
+          </c-table>
         </div>
         <button class="p-3 bg-[#A10E13] text-white hover:bg-red-600">Add</button>
       </div>
@@ -286,7 +214,7 @@
 import FileUploader from "@/components/FileUploader.vue";
 import CTable from '@/components/Datatable.vue'
 import { useNewRequestStore } from "@/modules/request/newrequest";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 const newRequestStore = useNewRequestStore();
 const multiple_input = ref(null);
 const view_items = ref(null);
@@ -300,10 +228,10 @@ const openModal = (modal) => {
   } else if (modal === "view_items") {
     view_items.value.showModal();
     view_items.value.classList.remove("-translate-y-5");
-  } else if (modal === 'new_unit'){
+  } else if (modal === 'new_unit') {
     new_unit.value.showModal()
     new_unit.value.classList.remove("-translate-y-5");
-  } else if (modal === 'search'){
+  } else if (modal === 'search') {
     search.value.showModal()
     search.value.classList.remove("-translate-y-5")
   }
@@ -316,12 +244,18 @@ const closeModal = (modal) => {
   } else if (modal === "view_items") {
     view_items.value.close();
     view_items.value.classList.add("-translate-y-5");
-  } else if (modal === 'new_unit'){
+  } else if (modal === 'new_unit') {
     new_unit.value.close()
     new_unit.value.classList.add("-translate-y-5");
-  } else if (modal === 'search'){
+  } else if (modal === 'search') {
     search.value.close()
     search.value.classList.add("-translate-y-5")
   }
 };
+
+onMounted(() => {
+  newRequestStore.setAgreementList().then(() => {
+    // console.log(response)
+  })
+})
 </script>
