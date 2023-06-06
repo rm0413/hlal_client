769f3b0e5797cb3139665df5efa48b9c274fc1e3
Sorisantos.r21 <reinamae.sorisantos@fujitsu.com>
2023-06-06 16:12:49 +0800

    [Request Form][Inspection Data] Added: #2994 Save Button - Single or Multiple Select Function

--- src/modules/request/inspectiondata.js
+++ src/modules/request/inspectiondata.js
@@ -16,8 +16,11 @@ export const useInspectionDataStore = defineStore({
             { label: 'Dimension', key: 'dimension' },
             { label: 'Critical Parts', key: 'critical_parts' },
             { label: 'Cpk', key: 'cpk_data' },
+            { label: 'Action', key: 'action' },
         ],
         inspectionDataForm: {
+            id: null,
+            agreement_id_pk: null,
             cpk_data: null,
             inspection_rework: null,
             revised_date: null,
@@ -39,20 +42,51 @@ export const useInspectionDataStore = defineStore({
                 })
             })
         },
-        clearInspectionData(){
-            this.inspectionDataForm = {
-                cpk_data: null,
-                inspection_rework: null,
-                revised_date: null,
-                send_date: null,
+        setInsertInspectionData(data) {
+            var payload = {
+                agreement_request_id: [],
+                cpk_data: this.inspectionDataForm.cpk_data,
+                inspection_after_rework: this.inspectionDataForm.inspection_rework,
+                revised_date_igm: this.inspectionDataForm.revised_date,
+                sent_date_igm: this.inspectionDataForm.send_date,
             }
+            data.forEach((v) => {
+                payload.agreement_request_id.push(v.agreement_id_pk)
+            })
+            return new Promise((resolve, reject) => {
+                axios.post('inspection-data', payload).then(response => {
+                    resolve(response.data)
+                    this.setInspectionDataRequest()
+                }).catch(err => {
+                    reject(err)
+                })
+            })
+        },
+        setUpdateInspectionData(){
+            var payload = {
+                id: this.inspectionDataForm.id,
+                agreement_request_id: this.inspectionDataForm.agreement_id_pk,
+                cpk_data: this.inspectionDataForm.cpk_data,
+                inspection_after_rework: this.inspectionDataForm.inspection_rework,
+                revised_date_igm: this.inspectionDataForm.revised_date,
+                sent_date_igm: this.inspectionDataForm.send_date,
+            }
+            console.log(payload)
+            return new Promise((resolve, reject) => {
+                axios.patch(`inspection-data/${payload.id}`, payload).then(response => {
+                    resolve(response.data)
+                    this.setInspectionDataRequest()
+                }).catch(err => {
+                    reject(err)
+                })
+            })
         }
     },
     getters: {
         getInspectionDataFields() {
             return this.inspectionDataFields
         },
-        getInspectionData(){
+        getInspectionData() {
             return this.inspectionData
         }
     }
--- src/views/request/InspectionData.vue
+++ src/views/request/InspectionData.vue
@@ -17,7 +17,7 @@
           <div class="relative">
             <i class="h-full z-50 text-gray-400 top-[2px] py-1 px-3 rounded absolute"><font-awesome-icon
                 icon="magnifying-glass"></font-awesome-icon></i>
-            <input class="text-center p-1 border-2 rounded-l-md" v-model="inspectionDataStore.search_filter"/>
+            <input class="text-center p-1 border-2 rounded-l-md" v-model="inspectionDataStore.search_filter" />
             <button class="h-full bg-gray-400 text-white py-1 px-3 rounded-r-md">
               Search
             </button>
@@ -30,32 +30,47 @@
     </div>
     <div class="h-[85vh] w-full grid grid-cols-9 min-[100px]:overflow-y-scroll lg:overflow-y-hidden gap-2">
       <div class="lg:col-span-7 min-[100px]:col-span-9 flex flex-col mt-2 h-[81vh] overflow-y-scroll">
-        <c-table ref="c-table" :isSelectable="true" @selectable="(data) => (select_data = data)"
+        <c-table ref="ctable" :isSelectable="true" @selectable="(data) => (select_data = data)"
           :fields="inspectionDataStore.getInspectionDataFields" :items="inspectionDataStore.getInspectionData"
-          :thStyle="'bg-[#A10E13] p-2 text-white'" :filter="inspectionDataStore.search_filter"></c-table>
+          :thStyle="'bg-[#A10E13] p-2 text-white'" :filter="inspectionDataStore.search_filter">
+          <template #cell(action)="data">
+            <button @click="editCpkData(data.item)" v-if="data.item.cpk_data !== null"
+              class="h-8 w-9 rounded bg-yellow-500 text-white" v-tooltip.top="'Edit Cpk Data'"><font-awesome-icon
+                icon="pen"></font-awesome-icon></button>
+            <button @click="editCpkData(data.item)" v-else class="h-8 w-9 rounded bg-yellow-500 text-white"
+              v-tooltip.top="'Edit Cpk Data'" hidden><font-awesome-icon icon="pen"></font-awesome-icon></button>
+          </template>
+        </c-table>
       </div>
       <div class="lg:col-span-2 min-[100px]:col-span-9 h-full flex flex-col mt-10 gap-2 px-10">
         <form method="post" @submit.prevent="submitInspectionData">
           <label class="flex flex-col items-center">
             <i class="text-gray-400">100% of Inspection Data or CPK Data</i>
-            <textarea style="resize:none" class="border-2 rounded w-full h-[10rem] text-center" v-model="inspectionDataStore.inspectionDataForm.cpk_data" required></textarea>
+            <textarea style="resize:none" class="border-2 rounded w-full h-[10rem] text-center"
+              v-model="inspectionDataStore.inspectionDataForm.cpk_data" required></textarea>
           </label>
           <label class="flex flex-col items-center">
             <i class="text-gray-400">Inspection after Rework</i>
-            <input type="text" class="border-2 rounded w-full h-[3rem] text-center" v-model="inspectionDataStore.inspectionDataForm.inspection_rework" required />
+            <input type="text" class="border-2 rounded w-full h-[3rem] text-center"
+              v-model="inspectionDataStore.inspectionDataForm.inspection_rework" required />
           </label>
           <label class="flex flex-col items-center">
             <i class="text-gray-400">Revised Date of IGM</i>
-            <input type="date" class="border-2 rounded w-full h-[3rem] text-center " v-model="inspectionDataStore.inspectionDataForm.revised_date" />
+            <input type="date" class="border-2 rounded w-full h-[3rem] text-center "
+              v-model="inspectionDataStore.inspectionDataForm.revised_date" />
           </label>
           <label class="flex flex-col items-center">
-            <i class="text-gray-400">Send Date of IGM</i>
-            <input type="date" class="border-2 rounded w-full h-[3rem] text-center " v-model="inspectionDataStore.inspectionDataForm.send_date" />
+            <i class="text-gray-400">Sent Date of IGM</i>
+            <input type="date" class="border-2 rounded w-full h-[3rem] text-center "
+              v-model="inspectionDataStore.inspectionDataForm.send_date" />
           </label>
-          <button type="submit"
-            class="flex gap-2 bg-[#A10E13] p-3 text-white rounded justify-center items-center w-full mt-2"><font-awesome-icon
+          <button type="submit" v-if="!inspectionDataStore.onEdit"
+            class="flex gap-2 bg-[#A10E13] hover:bg-red-600 p-3 text-white rounded justify-center items-center w-full mt-2"><font-awesome-icon
               icon="floppy-disk"></font-awesome-icon>Save</button>
-          <button type="button" @click="inspectionDataStore.clearInspectionData"
+          <button type="button" v-else @click="updateInspectionData"
+            class="flex gap-2 bg-yellow-400 p-3 hover:bg-yellow-200 text-black rounded justify-center items-center w-full mt-2"><font-awesome-icon
+              icon="floppy-disk"></font-awesome-icon>Update</button>
+          <button type="button" @click="clearInputs"
             class="flex gap-2 bg-gray-600 hover:bg-gray-500 p-3 text-white rounded justify-center items-center w-full mt-2">Clear</button>
         </form>
       </div>
@@ -79,9 +94,73 @@ onMounted(() => {
 
 const submitInspectionData = () => {
   if (select_data.value.length !== 0) {
-    console.log(true)
+    inspectionDataStore.setInsertInspectionData(select_data.value).then((response) => {
+      if (response.status === "success") {
+        clearInputs();
+        ctable.value.unSelect();
+        select_data.value = [];
+        swal({
+          icon: "success",
+          title: response.message,
+          timer: 1500
+        })
+      } else {
+        swal({
+          icon: "warning",
+          title: response.message,
+          timer: 1500
+        })
+      }
+    })
   } else {
     toast.add({ severity: 'error', summary: 'Warning', detail: 'Please select data in table', life: 2000, group: 'bl' });
   }
+}
+
+const editCpkData = (data) => {
+  ctable.value.unSelect();
+  select_data.value = [];
+  inspectionDataStore.onEdit = true;
+  inspectionDataStore.inspectionDataForm = {
+    id: data.inspection_id,
+    agreement_id_pk: data.agreement_request_id_fk,
+    cpk_data: data.cpk_data,
+    inspection_rework: data.inspection_after_rework,
+    revised_date: data.revised_date_igm,
+    send_date: data.sent_date_igm
+  }
+}
+
+const clearInputs = () => {
+  ctable.value.unSelect();
+  select_data.value = [];
+  inspectionDataStore.inspectionDataForm = {
+    cpk_data: null,
+    inspection_rework: null,
+    revised_date: null,
+    send_date: null,
+  }
+  inspectionDataStore.onEdit = false
+}
+
+const updateInspectionData = () => {
+  ctable.value.unSelect()
+  select_data.value = [];
+  inspectionDataStore.setUpdateInspectionData().then((response) => {
+    if (response.status === "success") {
+      clearInputs()
+      swal({
+        icon: "success",
+        title: response.message,
+        timer: 1500
+      })
+    } else {
+      swal({
+        icon: "warning",
+        title: response.message,
+        timer: 1500
+      })
+    }
+  })
 }
 </script>
