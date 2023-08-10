bee579efcfd2d9df5b9fb0b10a9808038d472321
athanathanthan <jdtrres@gmail.com>
2023-08-11 07:39:43 +0800

    [DASHBOARD] Updated: #3170 Count LSA and Hinsei Request: Count Request Result: Add date_from and date_to request

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,7 +12,8 @@
         <label
           class="flex col-span-1 text-center items-center justify-end italic mr-6"
         >
-          Today is {{ today }}
+          <font-awesome-icon class="h-5 w-5 mr-1 text-gray-700" icon="clock" />
+          {{ today }}
         </label>
       </div>
       <div
@@ -25,8 +26,60 @@
           >Welcome to Hinsei & LSA Agreement List System</i
         >
       </div>
-      <div class="py-3 px-12 font-bold text-[22px] mb-2">
-        <font-awesome-icon icon="chart-simple" /> Request Status
+      <div class="grid grid-cols-2 h-[5vh]">
+        <div class="col-span-1 py-3 px-12 font-bold text-[22px]">
+          <font-awesome-icon icon="chart-simple" /> Request Status
+        </div>
+        <div class="col-span-1 p-2">
+          <form method="post" class="h-full" @submit.prevent="submitDateFilter">
+            <div class="grid grid-cols-7 h-full gap-2">
+              <div
+                class="flex col-span-3 items-center justify-center text-white rounded-l"
+              >
+                <label
+                  class="flex w-[30%] justify-center bg-gray-500 h-full items-center rounded-l"
+                  >From:</label
+                >
+
+                <!-- <div class="col-span-2"> -->
+                <input
+                  type="date"
+                  v-model="dashboardStore.date_from"
+                  required
+                  class="rounded border-2 hover:border-red-600 outline-green-600 w-[70%] h-full text-center px-3 text-black"
+                  :max="max_date"
+                />
+                <!-- </div> -->
+              </div>
+              <div
+                class="flex col-span-3 items-center justify-center text-white rounded-l"
+              >
+                <label
+                  class="flex w-[30%] justify-center bg-gray-500 h-full items-center rounded-l"
+                  >To:</label
+                >
+
+                <!-- <div class="col-span-2"> -->
+                <input
+                  type="date"
+                  v-model="dashboardStore.date_to"
+                  required
+                  class="rounded border-2 hover:border-red-600 outline-green-600 w-[70%] h-full text-center px-3 text-black"
+                  :max="max_date"
+                />
+                <!-- </div> -->
+              </div>
+              <div class="col-span-1 bg-pink-200">
+                <button
+                  type="submit"
+                  class="w-full h-full bg-red-800 rounded text-white hover:bg-red-700 justify-center"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
       <label
         for=""
@@ -39,7 +92,7 @@
           ></b
         ></label
       >
-      <div class="grid grid-cols-2 h-[70%] mt-4">
+      <div class="grid grid-cols-2 h-[73%] mt-4">
         <div class="col-span-1 grid grid-rows-2">
           <div class="row-span-1">
             <Chart
@@ -67,69 +120,145 @@
             ></label
           >
         </div>
-        <div class="col-span-1 grid grid-rows-2">
-          <div class="col-span-1 flex flex-col gap-2 items-center">
-            <legend
-              class="flex col-span-2 text-center shadow-md justify-center items-center w-[80%] p-2 rounded bg-[#A10E13] text-white"
-            >
-              Date Filter
-            </legend>
-            <form
-              method="post"
-              class="w-[80%] p-2 grid grid-cols-2 p-3 gap-2 border-2 shadow-lg rounded-md bg-gray-100"
-              @submit.prevent="submitDateFilter"
-            >
-              <label for="" class="flex flex-col text-[14px] w-full">
-                From
-                <input
-                  type="date"
-                  v-model="dashboardStore.date_from"
-                  class="p-2 rounded border-2 hover:border-red-600 outline-green-600"
-                  required
-                  :max="max_date"
-                />
-              </label>
-              <label for="" class="flex flex-col text-[14px] w-full">
-                To
-                <input
-                  type="date"
-                  v-model="dashboardStore.date_to"
-                  required
-                  class="p-2 rounded border-2 hover:border-red-600 outline-green-600"
-                  :max="max_date"
-                />
-              </label>
-              <button
-                type="submit"
-                class="flex col-span-2 bg-red-800 w-[50%] p-2 rounded text-white hover:bg-red-700 mt-2 justify-center justify-self-center"
+        <div
+          class="col-span-1 grid grid-rows-2 justify-center items-center place-items-center"
+        >
+          <div
+            class="row-span-1 shadow-lg h-[25vh] w-[50vh] text-center p-3 bg-gray-100 border-t-2 border-[#A10A13]"
+          >
+            <span class="font-bold text-[18px] uppercase">Hinsei Request</span>
+            <div class="grid grid-cols-3">
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >OK</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ hinsei_ok }}</span
+                  >
+                </div>
+              </div>
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >NG</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ hinsei_ng }}</span
+                  >
+                </div>
+              </div>
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >PENDING</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ hinsei_pending }}</span
+                  >
+                </div>
+              </div>
+              <div
+                class="col-span-3 mt-3 bg-white w-full h-[6vh] rounded-full shadow-lg"
               >
-                Search
-              </button>
-            </form>
-          </div>
-
-          <div class="col-span-1 flex flex-col gap-8 items-center">
-            <div
-              class="col-span-1 flex justify-center items-center gap-2 h-[10vh] w-[25vw] bg-gray-200 rounded-full shadow-lg"
-            >
-              Total number of Hinsei Request
-              <button
-                @click="openModal('hinseiCount')"
-                class="w-[10rem] h-[2rem] border-blue-900 border-2 bg-blue-300 text-blue-800 rounded-full flex justify-center text-[14px] items-center font-bold"
-              >
-                {{ hinsei_count }}
-              </button>
+                <div class="grid grid-cols-2">
+                  <div
+                    class="flex col-span-1 h-[6vh] justify-center items-center"
+                  >
+                    Total Hinsei Request:
+                  </div>
+                  <button
+                    @click="openModal('hinseiCount')"
+                    class="flex col-span-1 h-[6vh] justify-center items-center font-bold text-[40px] text-blue-100 bg-blue-500 rounded-r-full"
+                  >
+                    {{ hinsei_count }}
+                  </button>
+                </div>
+              </div>
             </div>
-            <div
-              class="col-span-1 flex justify-center items-center gap-2 h-[10vh] w-[25vw] bg-gray-200 rounded-full shadow-lg"
-            >
-              Total number of LSA Request
-              <button
-                @click="openModal('lsaCount')"
-                class="w-[10rem] h-[2rem] border-orange-900 border-2 bg-orange-300 text-orange-800 rounded-full flex justify-center text-[14px] items-center font-bold"
+          </div>
+          <div
+            class="row-span-1 shadow-lg h-[25vh] w-[50vh] text-center p-3 bg-gray-100 border-t-2 border-[#A10A13]"
+          >
+            <span class="font-bold text-[18px] uppercase">LSA Request</span>
+            <div class="grid grid-cols-3">
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >OK</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ lsa_ok }}</span
+                  >
+                </div>
+              </div>
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >NG</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ lsa_ng }}</span
+                  >
+                </div>
+              </div>
+              <div class="flex col-span-1 justify-center items-center">
+                <div
+                  class="rounded-md bg-gray-100 w-[130px] h-[120px] border border-gray-500"
+                >
+                  <label
+                    for=""
+                    class="flex text-[20px] bg-[#A10A13] text-white justify-center"
+                    >PENDING</label
+                  >
+                  <span
+                    class="flex justify-center h-[74%] text-[60px] bg-white"
+                    >{{ lsa_pending }}</span
+                  >
+                </div>
+              </div>
+              <div
+                class="col-span-3 mt-3 bg-white w-full h-[6vh] rounded-full shadow-lg"
               >
-                {{ lsa_count }}
-              </button>
+                <div class="grid grid-cols-2">
+                  <div
+                    class="flex col-span-1 h-[6vh] justify-center items-center"
+                  >
+                    Total LSA Request:
+                  </div>
+                  <button
+                    @click="openModal('lsaCount')"
+                    class="flex col-span-1 h-[6vh] justify-center items-center font-bold text-[40px] text-orange-100 bg-orange-500 rounded-r-full"
+                  >
+                    {{ lsa_count }}
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -143,8 +272,100 @@
       >
         Task To Do!
       </div>
-      <div class="font-bold p-6 h-[35vh]">
-        Welcome to Hinsei & LSA Agreement List System
+      <div class="p-4 overflow-scroll w-[15.5vw] h-[53vh]">
+        <div class="flex flex-col space-y-3">
+          <!-- <div
+            class="grid grid-cols-7 rounded-lg border border-black shadow-xl select-none text-center"
+            v-for="(item, key) in task_to_do"
+            :key="key"
+          >
+            <div
+              class="flex col-span-1 justify-center bg-gray-600 rounded-l-md"
+            >
+              <font-awesome-icon
+                icon="bookmark"
+                class="self-center h-5 text-white"
+              />
+            </div>
+            <div class="col-span-6">
+              <span
+                class="font-mono text-[14px] font-bold"
+                v-tooltip.top="'13'"
+                >{{ item.value }}</span
+              >
+              <p>{{ item.req }}</p>
+            </div>
+          </div> -->
+          <div
+            class="grid grid-cols-7 rounded-lg border border-gray-800 shadow-xl select-none text-center h-[8vh]"
+          >
+            <div
+              class="flex col-span-1 justify-center bg-gray-800 rounded-l-md"
+            >
+              <font-awesome-icon
+                icon="bookmark"
+                class="self-center h-6 text-white"
+              />
+            </div>
+            <div class="col-span-6 self-center">
+              <span
+                class="font-mono text-[14px] font-bold"
+                v-tooltip.left="{
+                  value: `<b>PART NUMBER:</b> <br> KD23232323 <br> KD23232323 <br> KD23232323 <br> KD23232323`,
+                  escape: true,
+                }"
+                >Request needs to have CODE</span
+              >
+              <p>13</p>
+            </div>
+          </div>
+          <div
+            class="grid grid-cols-7 rounded-lg border border-gray-800 shadow-xl select-none text-center h-[8vh]"
+          >
+            <div
+              class="flex col-span-1 justify-center bg-gray-800 rounded-l-md"
+            >
+              <font-awesome-icon
+                icon="bookmark"
+                class="self-center h-6 text-white"
+              />
+            </div>
+            <div class="col-span-6 self-center">
+              <span
+                class="font-mono text-[14px] font-bold"
+                v-tooltip.left="{
+                  value: `<b>PART NUMBER:</b> <br> KD23232323 <br> KD23232323 <br> KD23232323 <br> KD23232323`,
+                  escape: true,
+                }"
+                >Request needs to have DESIGNER ANSWER</span
+              >
+              <p>13</p>
+            </div>
+          </div>
+          <div
+            class="grid grid-cols-7 rounded-lg border border-gray-800 shadow-xl select-none text-center h-[8vh]"
+          >
+            <div
+              class="flex col-span-1 justify-center bg-gray-800 rounded-l-md"
+            >
+              <font-awesome-icon
+                icon="bookmark"
+                class="self-center h-6 text-white"
+              />
+            </div>
+            <div class="col-span-6 self-center">
+              <span
+                class="font-mono text-[14px] font-bold"
+                v-tooltip.left="{
+                  value: `<b>PART NUMBER:</b> <br> KD23232323 <br> KD23232323 <br> KD23232323 <br> KD23232323`,
+                  escape: true,
+                }"
+                >Request that have CRITICAL PARTS
+              </span>
+              <p>13</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div
         class="font-bold outline-1 bg-[#A10E13] p-3 drop-shadow-xl text-white"
@@ -156,6 +377,11 @@
           <ul
             v-for="(i, key) in logs_activity"
             :key="key"
+            v-tooltip.left="{
+              value: `<div class='font-bold'>Activity Logs Details</div><div class='font-bold'>Agent:</div>${i.agent}<div class='font-bold'>IP:</div>${i.ip}`,
+              escape: true,
+              class: 'custom-error',
+            }"
             class="rounded-lg border border-black shadow-xl select-none text-center"
           >
             <span class="text-[13.5px] p-2 rounded font-mono font-bold">
@@ -322,12 +548,13 @@ import { onMounted, ref } from "vue";
 import { useDashboardStore } from "@/modules/dashboard.js";
 import moment from "moment";
 import { useToast } from "primevue/usetoast";
+import { TooltipComponent as EjsTooltip } from "@syncfusion/ej2-vue-popups";
 const toast = useToast();
 const dashboardStore = useDashboardStore();
 const max_date = moment().format("yyyy-MM-DD");
 const date_today = moment().format("yyyy-MM-DD");
 const startOfMonth = moment().startOf("month").format("yyyy-MM-DD");
-const today = moment().format("MMMM D , Y");
+const today = moment().format("[Today is ]dddd, MMMM Do YYYY");
 const hinseiCount = ref();
 const hinsei_ok = ref(null);
 const hinsei_ng = ref(null);
@@ -401,6 +628,9 @@ onMounted(() => {
   dashboardStore.date_to = date_today;
   submitDateFilter();
   chartOptions.value = setChartOptions();
+});
+
+const countRequest = () => {
   dashboardStore.setCountRequest().then((response) => {
     hinsei_count.value = response.data[0].hinsei_count;
     lsa_count.value = response.data[0].count_lsa;
@@ -411,7 +641,7 @@ onMounted(() => {
     lsa_ng.value = response.data[0].lsa_ng;
     lsa_pending.value = response.data[0].lsa_pending;
   });
-});
+};
 
 const logs_activity = ref([]);
 const showActivityLogs = () => {
@@ -423,11 +653,20 @@ const showActivityLogs = () => {
         name: v.name,
         subject: v.subject,
         date: v.created_at,
+        agent: v.agent,
+        ip: v.ip,
+        url: v.url,
       };
       logs_activity.value.push(datastorage);
     });
   });
 };
+
+// const task_to_do = ref([
+//   { value: "Request needs to have CODE", req: "13" },
+//   { value: "Request needs to have DESIGNER ANSWER", req: "13" },
+//   { value: "Request that have CRITICAL PARTS", req: "13" },
+// ]);
 
 const chartOptions = ref();
 
@@ -445,6 +684,7 @@ const setChartOptions = () => {
     aspectRatio: 0.8,
     plugins: {
       legend: {
+        display: false,
         labels: {
           fontColor: textColor,
         },
@@ -478,6 +718,8 @@ const setChartOptions = () => {
 
 const submitDateFilter = () => {
   showActivityLogs();
+  countRequest();
+  dashboardStore.setCountRequest();
   dashboardStore.setLoadCountResult().then((response) => {
     if (response.status === "success") {
       chartData.value = {
@@ -532,3 +774,15 @@ const submitDateFilter = () => {
   });
 };
 </script>
+<style>
+.custom-error {
+  padding: 0;
+}
+.custom-error .p-tooltip-text {
+  color: white;
+  justify-content: center;
+}
+/* .custom-error .p-tooltip-left .p-tooltip-arrow {
+  border-right-color: var(--red-800);
+} */
+</style>
